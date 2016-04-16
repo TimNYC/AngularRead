f4b3d17cd995f734c998ad55775b169327343499
Tim Wang <timwnewyork@gmail.com>
2016-04-15 23:54:28 -0400

    Update helper.js

--- 2000/helper.js
+++ 2000/helper.js
@@ -19,3 +19,41 @@ function isElement(node) {
 function toInt(str) {
   return parseInt(str, 10);
 }
+
+function identity($) {return $;}
+function noop() {}
+
+!!
+function isWindow(obj) {
+  return obj && obj.window === obj;
+}
+
+
+function isScope(obj) {
+  return obj && obj.$evalAsync && obj.$watch;
+}
+
+
+function isFile(obj) {
+  return toString.call(obj) === '[object File]';
+}
+
+
+function isFormData(obj) {
+  return toString.call(obj) === '[object FormData]';
+}
+
+
+function isBlob(obj) {
+  return toString.call(obj) === '[object Blob]';
+}
+
+
+function isBoolean(value) {
+  return typeof value === 'boolean';
+}
+
+
+function isPromiseLike(obj) {
+  return obj && isFunction(obj.then);
+}
